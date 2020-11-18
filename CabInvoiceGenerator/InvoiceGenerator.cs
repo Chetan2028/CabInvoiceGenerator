4434764965fff853fa25dec9aff95262f8e9780f
Chetan2028 <bmchetan2028@gmail.com>
2020-11-18 21:45:40 +0530

    [Chetan] Add . Added to calculate fare

---
+++ CabInvoiceGenerator/InvoiceGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGenerator
+{
+    class InvoiceGenerator
+    {
+        /// Constants
+        public const int MINIMUM_FARE = 5;
+        public const int COST_PER_KM = 10;
+        public const int COST_PER_TIME = 1;
+
+        /// <summary>
+        /// Calculates the fare.
+        /// </summary>
+        /// <param name="distance">The distance.</param>
+        /// <param name="time">The time.</param>
+        /// <returns></returns>
+        public double CalculateFare(double distance , int time)
+        {
+            double totalFare = distance * COST_PER_KM + COST_PER_TIME * time;
+            return Math.Max(totalFare, MINIMUM_FARE);
+        }
+        
+    }
+}
--- CabInvoiceGenerator/Program.cs
+++ CabInvoiceGenerator/Program.cs
@@ -6,7 +6,14 @@ namespace CabInvoiceGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Cab Invoice Generator");
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            Console.WriteLine("Enter the distance");
+            double distance =Convert.ToDouble( Console.ReadLine());
+            Console.WriteLine("Enter the time");
+            int time = Convert.ToInt32(Console.ReadLine());
+            double totalFare = invoiceGenerator.CalculateFare(distance,time);
+            Console.WriteLine("Total Fare : {0}",totalFare);
         }
     }
 }
